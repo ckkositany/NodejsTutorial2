97581b456deeab7ced75aeb65074aae083aed691
Kositanyck <kositanyck@gmail.com>
2023-09-11 14:25:03 +0300

    CCreating the add-story route and requiring it

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ app.use(express.static(path.join(__dirname,'public')))
 //routes
 app.use('/',require('./routes/index'))
 app.use('/auth',require('./routes/auth'))
+app.use('/stories',require('./routes/stories'))
+
 
 const PORT = process.env.PORT || 3000
 //console.log(process.env.MONGO_URI)
