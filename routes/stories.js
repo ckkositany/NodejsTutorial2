48a25ab3ae9d62cef51602555fe33348a795d5fa
Kositanyck <kositanyck@gmail.com>
2023-09-29 12:09:35 +0300

    Adding env files to gitignore

--- routes/stories.js
+++ routes/stories.js
@@ -141,5 +141,25 @@ router.delete('/:id', ensureAuth, async (req,res)=>{
     res.render('error/500')
   }
 })
+//@description user stories
+// @ route GET /stories/user/:userId
+router.get('/user/:userId ', ensureAuth, async (req,res)=>{
+  try {
+  const stories =await Story.find({
+    user: req.params.userId,
+    status: 'public',
+  })
+  .populate('user')
+  .lean()
+  console.log(user)
+  res.render('stories/index',{
+    stories,
+  })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 
 module.exports=router
